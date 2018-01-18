32e73ceb9e57fa0708c0e7473b6c2603ad8aa212
tophatGrizzly <LJK150030@UTDallas.edu>
2018-01-17 19:18:04 -0600

    1/17/18
    Board generator is where I would like it to be, along with adjacency list. However, I think there is some redudency with the structer and will need to fix this.

--- PathfindingAI/Assets/Scripts/BoardGen.cs
+++ PathfindingAI/Assets/Scripts/BoardGen.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 public class BoardGen : MonoBehaviour
 {
-
+    public Camera Camera;
     public FloorTile FloorTileGameObject;
     public int BoardWidth;
     public int BoardHeight;
 
     private Dictionary<int, FloorTile> _adjacencyList;
     private FloorTile[,] _board;
+    private RaycastHit _hit;
+    private FloorTile _startTile;
+    private FloorTile _goalTile;
+    private bool _goalToStart;
 
     //setting up the board and the connections
     private void Awake()
@@ -18,6 +22,94 @@ public class BoardGen : MonoBehaviour
         _board = new FloorTile[BoardWidth, BoardHeight];
 
         GenerateBoard();
+        GenerateCardinalEdges();
+        _goalToStart = false;
+        _startTile = null;
+        _goalTile = null;
+    }
+
+    private void Update()
+    {
+        //left click changes a tile from active to deactive
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 16.0f);
+
+            if (hit.collider != null)
+            {
+                var currentTile = hit.collider.gameObject.GetComponent<FloorTile>();
+
+                if (_board[currentTile.Xlocation, currentTile.Ylocation].Equals(_startTile))
+                    _startTile = null;
+
+                if (_board[currentTile.Xlocation, currentTile.Ylocation].Equals(_goalTile))
+                    _goalTile = null;
+
+                //changing the type and updating adjacency list
+                if (_board[currentTile.Xlocation, currentTile.Ylocation].type != 0)
+                {
+                    _board[currentTile.Xlocation, currentTile.Ylocation].type = 0;
+                    RemoveCardinalEdges(currentTile.Xlocation, currentTile.Ylocation);
+                }
+                else
+                {
+                    _board[currentTile.Xlocation, currentTile.Ylocation].type = 1;
+                    AddCardinalEdges(currentTile.Xlocation, currentTile.Ylocation);
+                }
+
+                _board[currentTile.Xlocation, currentTile.Ylocation].UpdateSprite();
+            }
+        }
+
+        //right click changes the start and goal tiles
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 16.0f);
+
+            if (hit.collider != null)
+            {
+                var currentTile = hit.collider.gameObject.GetComponent<FloorTile>();
+
+                if (_startTile != null && _goalTile != null)
+                {
+                    if (!_goalToStart)
+                    {
+                        _startTile.type = 1;
+                        _startTile.UpdateSprite();
+                        _board[currentTile.Xlocation, currentTile.Ylocation].type = 2;
+                        _startTile = _board[currentTile.Xlocation, currentTile.Ylocation];
+                        _startTile.UpdateSprite();
+                        _goalToStart = true;
+                    }
+                    else
+                    {
+                        _goalTile.type = 1;
+                        _goalTile.UpdateSprite();
+                        _board[currentTile.Xlocation, currentTile.Ylocation].type = 3;
+                        _goalTile = _board[currentTile.Xlocation, currentTile.Ylocation];
+                        _goalTile.UpdateSprite();
+                        _goalToStart = false;
+                    }
+                }
+
+                if (_startTile == null)
+                {
+                    _board[currentTile.Xlocation, currentTile.Ylocation].type = 2;
+                    _startTile = _board[currentTile.Xlocation, currentTile.Ylocation];
+                    _startTile.UpdateSprite();
+                    _goalToStart = true;
+                    return;
+                }
+
+                if (_goalTile == null)
+                {
+                    _board[currentTile.Xlocation, currentTile.Ylocation].type = 3;
+                    _goalTile = _board[currentTile.Xlocation, currentTile.Ylocation];
+                    _goalTile.UpdateSprite();
+                    _goalToStart = false;
+                }
+            }
+        }
     }
 
     //Generates the board and populates the adjacency list with vertices
@@ -33,7 +125,7 @@ public class BoardGen : MonoBehaviour
             {
                 _board[x,y] = Instantiate(FloorTileGameObject, currentLocation, Quaternion.identity, transform);
                 currentLocation = new Vector2(currentLocation.x, currentLocation.y - 1.0f);
-                _board[x, y].ActivateTile(numNodes);
+                _board[x, y].ActivateTile(numNodes, x, y);
                 AddVertex(numNodes, _board[x, y]);
                 numNodes++;
             }
@@ -42,19 +134,20 @@ public class BoardGen : MonoBehaviour
         }
     }
 
-    /*
-    private void GenerateEdges()
+    //sets up adjacency list with North, East, South, West
+    private void GenerateCardinalEdges()
     {
+        //x
         for (int i = 0; i < BoardWidth; i++)
         {
+            //y
             for (int j = 0; j < BoardHeight; j++)
             {
-                
+                AddCardinalEdges(i, j);
             }
         }
     }
-    */
-
+    
     //adds an instance of id to the graph
     private void AddVertex(int id, FloorTile tile)
     {
@@ -82,7 +175,7 @@ public class BoardGen : MonoBehaviour
     //Adds a new, directed edge to the graph that connects two vertices.
     private void AddEdge(int fromVert, int toVert)
     {
-        if (!_adjacencyList.ContainsKey(fromVert) && !_adjacencyList.ContainsKey(toVert))
+        if (_adjacencyList.ContainsKey(fromVert) && _adjacencyList.ContainsKey(toVert))
         {
             _adjacencyList[fromVert].Adj.Add(toVert, 1.0f);
             Debug.Log("G(" + fromVert + "," + toVert + ")");
@@ -91,10 +184,11 @@ public class BoardGen : MonoBehaviour
             Debug.Log("Vertices " + fromVert + " to " + toVert + " does not exits");
     }
 
+    /*
     //Adds a new, weighted, directed edge to the graph that connects two vertices.
     private void AddEdge(int fromVert, int toVert, float weight)
     {
-        if (!_adjacencyList.ContainsKey(fromVert) && !_adjacencyList.ContainsKey(toVert))
+        if (_adjacencyList.ContainsKey(fromVert) && _adjacencyList.ContainsKey(toVert))
         {
             _adjacencyList[fromVert].Adj.Add(toVert, weight);
             Debug.Log("G(" + fromVert + "," + toVert + "," + weight + ")");
@@ -102,6 +196,8 @@ public class BoardGen : MonoBehaviour
         else
             Debug.Log("Vertices " + fromVert + " to " + toVert + " does not exits");
     }
+    */
+
 
     private void RemoveEdge(int fromVert, int toVert)
     {
@@ -111,7 +207,7 @@ public class BoardGen : MonoBehaviour
             Debug.Log("Removed G(" + fromVert + "," + toVert + ")");
         }
         else
-            Debug.Log("Vertices " + fromVert + " to " + toVert + " does not exits");
+            Debug.Log("Cannot remove vertices " + fromVert + " to " + toVert);
     }
 
     //finds the vertex in the graph named vertKey
@@ -138,6 +234,60 @@ public class BoardGen : MonoBehaviour
         {
             Debug.Log("Vertices " + fromVert + " to " + toVert + " does not exits");
             return -1.0f;
+        }
+    }
+
+    private void AddCardinalEdges(int x, int y)
+    {
+        //12 o'clock
+        if ((y - 1) >= 0)
+            if (_board[x, y - 1].type != 0)
+                AddEdge(_board[x, y].Id, _board[x, y - 1].Id);
+
+        //3 o'clock
+        if ((x + 1) < BoardWidth)
+            if (_board[x + 1, y].type != 0)
+                AddEdge(_board[x, y].Id, _board[x + 1, y].Id);
+
+        //6 o'clock
+        if ((y + 1) < BoardHeight)
+            if (_board[x, y + 1].type != 0)
+                AddEdge(_board[x, y].Id, _board[x, y + 1].Id);
+
+        //9 o'clock
+        if ((x - 1) >= 0)
+            if (_board[x - 1, y].type != 0)
+                AddEdge(_board[x, y].Id, _board[x - 1, y].Id);
+    }
+
+    private void RemoveCardinalEdges(int x, int y)
+    {
+        //12 o'clock
+        if ((y - 1) >= 0)
+        {
+            RemoveEdge(_board[x, y].Id, _board[x, y - 1].Id);
+            RemoveEdge(_board[x, y - 1].Id, _board[x, y].Id);
+        }
+
+        //3 o'clock
+        if ((x + 1) < BoardWidth)
+        {
+            RemoveEdge(_board[x, y].Id, _board[x + 1, y].Id);
+            RemoveEdge(_board[x + 1, y].Id, _board[x, y].Id);
+        }
+
+        //6 o'clock
+        if ((y + 1) < BoardHeight)
+        {
+            RemoveEdge(_board[x, y].Id, _board[x, y + 1].Id);
+            RemoveEdge(_board[x, y + 1].Id, _board[x, y].Id);
+        }
+
+        //9 o'clock
+        if ((x - 1) >= 0)
+        {
+            RemoveEdge(_board[x, y].Id, _board[x - 1, y].Id);
+            RemoveEdge(_board[x - 1, y].Id, _board[x, y].Id);
         }
     }
 }
--- PathfindingAI/Assets/Scripts/FloorTile.cs
+++ PathfindingAI/Assets/Scripts/FloorTile.cs
@@ -5,12 +5,38 @@ public class FloorTile : MonoBehaviour
 {
     public int Id;
     public Dictionary<int, float> Adj;
-    public bool Active;
+    public int type; // 0 (deactive), 1 (active), 2 (start), 3 (goal)
+    public int Xlocation;
+    public int Ylocation;
 
-    public void ActivateTile(int id)
+    private SpriteRenderer _render;
+
+    public void ActivateTile(int id, int x, int y)
     {
         Id = id;
-        Active = true;
+        type = 1;
+        Xlocation = x;
+        Ylocation = y;
         Adj = new Dictionary<int, float>();
+        _render = GetComponent<SpriteRenderer>();
+    }
+
+    public void UpdateSprite()
+    {
+        switch (type)
+        {
+            case 0:
+                _render.color = Color.black;
+                break;
+            case 1:
+                _render.color = Color.white;
+                break;
+            case 2:
+                _render.color = Color.green;
+                break;
+            case 3:
+                _render.color = Color.red;
+                break;
+        }
     }
 }
